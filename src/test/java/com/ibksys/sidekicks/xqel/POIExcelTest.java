9e94b413d1766c0446b2465fec25db33a3c10a23
SeungyoonLee <samsee@ibksystem.co.kr>
2016-08-10 17:48:24 +0900

    Add poi dependencies and test stub

---
+++ src/test/java/com/ibksys/sidekicks/xqel/POIExcelTest.java
@@ -0,0 +1,14 @@
+package com.ibksys.sidekicks.xqel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class POIExcelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
