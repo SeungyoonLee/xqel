ebc12334ba79e2bb1bcf958c165319f8b881ecac
SeungyoonLee <samsee@ibksystem.co.kr>
2016-08-10 16:15:55 +0900

    Initial Commit

---
+++ xqel/src/main/java/com/ibksys/sidekicks/xqel/XqelRunner.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.ibksys.sidekicks.xqel;
+
+/**
+ * @author Seungyoon Lee <samsee@ibksystem.co.kr>
+ *
+ */
+public class XqelRunner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		help();
+	}
+
+	static void help() {
+		System.out.println("Usage : " + XqelRunner.class + " classToRun params..");
+	}
+}
